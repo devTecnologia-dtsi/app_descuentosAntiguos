ea6448b757485a575f19f0a4f8301dcb126475f1
Diego Espitia <diegoalexander91@hotmail.com>
2024-10-21 07:19:40 -0500

    Se agregan variables de entorno

--- Frontend/src/app/environments/environment.ts
+++ Frontend/src/app/environments/environment.ts
@@ -1,6 +1,13 @@
+// export const environment = {
+//     production: false,
+//     apiUrl: 'http://localhost:80',
+//     apiDatosPersonales: 'https://uniminuto.api.digibee.io/pipeline/uniminuto/v1/servicios-banner/datosPersonales',
+//     key: 'ITnjVcrLWfYpY2B246EcrWO6Hln3LD7a',
+//     consultarCuenta: 'https://uniminuto.api.digibee.io/pipeline/uniminuto/v1/directorio-activo/consultarCuenta'
+// };
 export const environment = {
-    production: false,
-    apiUrl: 'http://localhost:80',
+    production: true,
+    apiUrl: 'http://backend:8083',
     apiDatosPersonales: 'https://uniminuto.api.digibee.io/pipeline/uniminuto/v1/servicios-banner/datosPersonales',
     key: 'ITnjVcrLWfYpY2B246EcrWO6Hln3LD7a',
     consultarCuenta: 'https://uniminuto.api.digibee.io/pipeline/uniminuto/v1/directorio-activo/consultarCuenta'
