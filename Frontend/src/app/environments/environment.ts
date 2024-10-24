222138af6620a229d0301edc985c483bcfe717be
Diego Espitia <diegoalexander91@hotmail.com>
2024-10-24 16:12:57 -0500

    Se agrega configuración de nginx

--- Frontend/src/app/environments/environment.ts
+++ Frontend/src/app/environments/environment.ts
@@ -7,7 +7,7 @@
 // };
 export const environment = {
     production: true,
-    apiUrl: 'http://backend:80',
+    apiUrl: '/api',
     apiDatosPersonales: 'https://uniminuto.api.digibee.io/pipeline/uniminuto/v1/servicios-banner/datosPersonales',
     key: 'ITnjVcrLWfYpY2B246EcrWO6Hln3LD7a',
     consultarCuenta: 'https://uniminuto.api.digibee.io/pipeline/uniminuto/v1/directorio-activo/consultarCuenta'
